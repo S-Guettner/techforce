85d6df5bf2aec200475c6191c40a1b9f4055cfa4
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-05-24 18:12:48 +0200

    feat: added nav in layout

---
+++ app/bussines/page.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react'
+
+interface pageProps {
+  
+}
+
+const page: FC<pageProps> = ({}) => {
+  return (
+    <div>
+        <h1>Bussines area</h1>
+    </div>
+  )
+}
+
+export default page
