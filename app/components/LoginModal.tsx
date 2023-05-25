8e8fb152c0b1128a7adaa889584126edb743f2b4
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-05-25 15:50:01 +0200

    feat: added modals for login && registration

---
+++ app/components/LoginModal.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+
+interface LoginModalProps {
+  
+}
+
+const LoginModal: FC<LoginModalProps> = ({}) => {
+  return <div>LoginModal</div>
+}
+
+export default LoginModal
---
+++ app/components/RegistrationModal.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+
+interface RegistrationModalProps {
+  
+}
+
+const RegistrationModal: FC<RegistrationModalProps> = ({}) => {
+  return <div>RegistrationModal</div>
+}
+
+export default RegistrationModal
