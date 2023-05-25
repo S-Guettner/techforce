18285079ee0ec52651fa96fb3d052dd48c525d44
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-05-26 01:45:09 +0200

    feat: adding connection to db

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,59 +1,27 @@
 import NextAuth from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import EmailProvider from 'next-auth/providers/email';
-import NodeMailer from 'nodemailer'
+import NodeMailer from 'nodemailer';
 import GitHubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
 
+
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID as string;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET as string;
+
 const handler = NextAuth({
     providers: [
-        CredentialsProvider({
-            // The name to display on the sign in form (e.g. 'Sign in with...')
-            name: 'Credentials',
-            // The credentials is used to generate a suitable form on the sign in page.
-            // You can specify whatever fields you are expecting to be submitted.
-            // e.g. domain, username, password, 2FA token, etc.
-            // You can pass any HTML attribute to the <input> tag through the object.
-            credentials: {
-                username: { label: 'Email', type: 'email'},
-                password: { label: 'Password', type: 'password' },
-            },
-            async authorize(credentials, req) {
-                // You need to provide your own logic here that takes the credentials
-                // submitted and returns either a object representing a user or value
-                // that is false/null if the credentials are invalid.
-                // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
-                // You can also use the `req` object to obtain additional parameters
-                // (i.e., the request IP address)
-                const res = await fetch('/your/endpoint', {
-                    method: 'POST',
-                    body: JSON.stringify(credentials),
-                    headers: { 'Content-Type': 'application/json' },
-                });
-                const user = await res.json();
-
-                // If no error and we have user data, return it
-                if (res.ok && user) {
-                    return user;
-                }
-                // Return null if user data could not be retrieved
-                return null;
-            },
-        }),
-/*         EmailProvider({
-            server: "process.env.EMAIL_SERVER",
-            from: "process.env.EMAIL_FROM",
-            // maxAge: 24 * 60 * 60, // How long email links are valid for (default 24h)
-        }), */
         GoogleProvider({
-            clientId: "process.env.GOOGLE_CLIENT_ID",
-            clientSecret: "process.env.GOOGLE_CLIENT_SECRET",
-        }),
-        GitHubProvider({
-            clientId: "process.env.GITHUB_ID",
-            clientSecret: "process.env.GITHUB_SECRET",
+            clientId: GOOGLE_CLIENT_ID,
+            clientSecret: GOOGLE_CLIENT_SECRET,
         }),
     ],
+    /*     async session({session}){
+
+    },
+    async signIn({profile}){
+
+    } */
 });
 
 export { handler as GET, handler as POST };
