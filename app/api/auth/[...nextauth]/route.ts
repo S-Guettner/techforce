f07b88c4af2cf71d925c2e5e2ff72aeb496dd9b1
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-05-29 18:50:02 +0200

    feat: able to save user that signs up with google

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,11 @@
 import NextAuth, { AuthOptions, SessionOptions } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+import mongoose from 'mongoose';
+import userModel from '../../../utils/models/userModel';
+import { connectionToDB } from '@/app/utils/database';
 
-const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID as string
-const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET as string
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID as string;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET as string;
 
 const handler = NextAuth({
     providers: [
@@ -11,7 +14,24 @@ const handler = NextAuth({
             clientSecret: GOOGLE_CLIENT_SECRET,
         }),
     ],
-    secret:process.env.JWT_SECRET
+    secret: process.env.JWT_SECRET,
+    callbacks: {
+        async signIn({ user, account, profile, email, credentials }) {
+            await connectionToDB();
+
+            try {
+                // Perform any additional logic or database operations here
+                await userModel.create({
+                    userName: user.name,
+                    email: user.email,
+                });
+                return true; // Return true to indicate successful sign-in
+            } catch (error) {
+                console.log(error);
+                return false;
+            }
+        },
+    },
 });
 
 export { handler as GET, handler as POST };
