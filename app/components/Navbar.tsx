3f649af7b966e4d2df6cc7cb4eaefaa9046db52e
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-06-19 21:21:20 +0200

    feat: added signout button

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -17,7 +17,7 @@ const Navbar: FC<NavbarProps> = () => {
     const [providers, setProviders] = useState<Record<LiteralUnion<BuiltInProviderType, string>, ClientSafeProvider> | null>()
     console.log(providers)
 
-    const {data: session} = useSession()
+    const { data: session } = useSession()
 
     useEffect(() => {
         const providers = async () => {
@@ -33,18 +33,27 @@ const Navbar: FC<NavbarProps> = () => {
         <nav>
             {providers && Object.values(providers).map((provider) => {
                 return (
+                    <nav key={provider.id}>
 
-                    <button
-                    type='button'
-                    key={provider.name}
-                    onClick={() => signIn(provider.id)}
-                    >
-                        Sign in
-                    </button>
+                        <button
+                            type='button'
+
+                            onClick={() => signIn(provider.id)}
+                        >
+                            Sign in
+                        </button>
+
+                        <button className='mx-5' type='button'
+
+                            onClick={() => signOut()}>
+                                Sign Out
+                        </button>
+
+                    </nav>
 
                 )
             })}
-        </nav> 
+        </nav>
     )
 }
 
