ae972998140f4ec6a9d3ef55eefd6bd3de99b6ae
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-06-29 14:55:14 +0200

    added sign up modal

--- app/components/RegistrationModal.tsx
+++ app/components/RegistrationModal.tsx
@@ -12,7 +12,9 @@ const RegistrationModal: FC<RegistrationModalProps> = ({ setModalStatus }) => {
         <h2>Sign Up</h2>
         <button onClick={() => setModalStatus(prev => !prev)}><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1.3em" width="1.3em" xmlns="http://www.w3.org/2000/svg"><path fill="none" stroke="#000" stroke-width="2" d="M3,3 L21,21 M3,21 L21,3"></path></svg></button>
       </div>
-
+      <div>Sign up with email</div>
+      <div>sign up with google</div>
+      <div>sign up with github</div>
     </div>
 
   </div>
