eb696a7b6dee8e3cd1a4ce886b8c6bd0866dcc7c
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-06-29 15:51:27 +0200

    adding nodemailer

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,22 @@
 import NextAuth, { AuthOptions, SessionOptions } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 import GitHubProvider from 'next-auth/providers/github';
+import EmailProvider from 'next-auth/providers/email';
 import userTest from '@/app/utils/models/userModel';
 import connectionToDB from '@/app/utils/database';
 
+/* google auth */
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID as string;
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET as string;
 
+/* github auth */
 const GITHUB_ID = process.env.GITHUB_ID as string;
 const GITHUB_SECRET = process.env.GITHUB_SECRET as string;
+
+/* mail magic link auth */
+const EMAIL_SERVER_HOST = process.env.EMAIL_SERVER_HOST as string
+const EMAIL_SERVER_PORT = process.env.EMAIL_SERVER_PORT ? parseInt(process.env.EMAIL_SERVER_PORT) : undefined
+
 
 const handler = NextAuth({
     providers: [
@@ -19,6 +27,17 @@ const handler = NextAuth({
         GitHubProvider({
             clientId: GITHUB_ID,
             clientSecret: GITHUB_SECRET,
+        }),
+        EmailProvider({
+            server: {
+                host: EMAIL_SERVER_HOST,
+                port: EMAIL_SERVER_PORT,
+                auth: {
+                    user: process.env.EMAIL_SERVER_USER,
+                    pass: process.env.EMAIL_SERVER_PASSWORD,
+                },
+            },
+            from: process.env.EMAIL_FROM,
         }),
     ],
     secret: process.env.JWT_SECRET,
@@ -36,9 +55,9 @@ const handler = NextAuth({
                     await userTest.create({
                         email: userEmail,
                     });
-                    console.log("User not in DB")
-                }else{
-                    console.log("User already in DB")
+                    console.log('User not in DB');
+                } else {
+                    console.log('User already in DB');
                 }
 
                 return true;
