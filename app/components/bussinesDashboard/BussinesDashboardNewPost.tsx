5dd09528de6b57fe4d4b23507ec037bf3fd296c1
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-07-03 18:08:34 +0200

    bussines dashboard new post preview

--- app/components/bussinesDashboard/BussinesDashboardNewPost.tsx
+++ app/components/bussinesDashboard/BussinesDashboardNewPost.tsx
@@ -8,8 +8,15 @@ interface BussinesDashboardNewPostProps {
 
 const BussinesDashboardNewPost: FC<BussinesDashboardNewPostProps> = ({ }) => {
 
+
   const [tasks, setTasks] = useState<string[]>([]);
   const [task, setTask] = useState("")
+
+  const [offers, setOffers] = useState<string[]>([])
+  const [offer, setOffer] = useState("")
+
+  const [requirements, setRequirements] = useState<string[]>([])
+  const [requirement, setRequirement] = useState("")
 
   const addTask = (task: string) => {
     setTasks((prevTasks) => [...prevTasks, task]);
@@ -21,9 +28,29 @@ const BussinesDashboardNewPost: FC<BussinesDashboardNewPostProps> = ({ }) => {
     setTasks(newTasks);
   };
 
+  const addOffer = (offer: string) => {
+    setOffers((prevOffers) => [...prevOffers, offer]);
+  };
+
+  const deleteOffer = (index: number) => {
+    const newOffers = [...offers];
+    newOffers.splice(index, 1);
+    setOffers(newOffers);
+  };
+
+  const addRequirement = (requirement: string) => {
+    setRequirements((prevRequirements) => [...prevRequirements, requirement]);
+  };
+
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault(); // Prevent the default form submission behavior
     // Process the form data or perform any necessary actions
+  };
+
+  const deleteRequirement = (index: number) => {
+    const newRequirements = [...requirements];
+    newRequirements.splice(index, 1);
+    setRequirements(newRequirements);
   };
 
   return (
@@ -44,23 +71,64 @@ const BussinesDashboardNewPost: FC<BussinesDashboardNewPostProps> = ({ }) => {
           <button onClick={() => addTask(task)}>add task</button>
         </form>
       </div>
-      <input className='block border-2 border-black' placeholder='What we offer you' type="text" name="jobOffer" id="jobOffer" />
-      <input className='block border-2 border-black' placeholder='job requirements' type="text" name="jobRequirements" id="jobRequirements" />
+      <div>
+        <input onChange={(e) => setOffer(e.target.value)} className='block border-2 border-black' placeholder='What we offer you' type="text" name="jobOffer" id="jobOffer" />
+        <button onClick={() => addOffer(offer)}>add offer</button>
+      </div>
+      <div>
+        <input onChange={(e) => setRequirement(e.target.value)} className='block border-2 border-black' placeholder='job requirements' type="text" name="jobRequirements" id="jobRequirements" />
+        <button onClick={() => addRequirement(requirement)}>add requirement</button>
+      </div>
 
 
-      <section>
-        <p>Preview</p>
+      <section className='bg-white h-screen'>
         <div>
-          {tasks && tasks.map((item, index) => {
-            return (
-              <div>
-                <p className='inline'>&#x2022;{item} index = {index}</p>
-                <button className='bg-red-400 p-1 text-white rounded-md' onClick={() => deleteTask(index)}>delete task</button>
-              </div>
-            )
-          }
+          {/* ========================================================= TASKS ============================================================ */}
+          <p>Tasks</p>
+          <div>
+            {tasks && tasks.map((item, index) => {
+              return (
+                <div key={nanoid()}>
+                  <p className='inline'>&#x2022;{item} index = {index}</p>
+                  <button className='bg-red-400 p-1 text-white rounded-md' onClick={() => deleteTask(index)}>delete task</button>
+                </div>
+              )
+            }
 
-          )}
+            )}
+          </div>
+        </div>
+        <div>
+          {/* ========================================================= OFFERS ============================================================ */}
+          <p>Job offer</p>
+          <div>
+            {offers && offers.map((item, index) => {
+              return (
+                <div key={nanoid()}>
+                  <p className='inline'>&#x2022;{item} index = {index}</p>
+                  <button className='bg-red-400 p-1 text-white rounded-md' onClick={() => deleteOffer(index)}>delete offer</button>
+                </div>
+              )
+            }
+
+            )}
+          </div>
+        </div>
+        <div>
+          {/* ========================================================= REQUIREMENTS ============================================================ */}
+          <p>Job requirement</p>
+          <div>
+            {requirements && requirements.map((item, index) => {
+              return (
+                <div key={nanoid()}>
+                  <p className='inline'>&#x2022;{item} index = {index}</p>
+                  <button className='bg-red-400 p-1 text-white rounded-md' onClick={() => deleteRequirement(index)}>delete requirement</button>
+                </div>
+              )
+            }
+
+            )}
+          </div>
         </div>
       </section>
     </main>
