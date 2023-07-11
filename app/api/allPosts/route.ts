7606dde1df3d6544a9577f4ac8597f343a9e5cf5
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-07-11 17:11:50 +0200

    toDo: all posts route throws error

--- app/api/allPosts/route.ts
+++ app/api/allPosts/route.ts
@@ -1,12 +1,12 @@
-import { NextResponse } from 'next/server';
+import { NextResponse, NextRequest } from 'next/server';
 import testUser from '@/app/utils/models/userModel';
 
-export const GET = async (req: Request, res: Response) => {
+export const GET = async (req: Request, res: NextResponse) => {
     try {
         const jobPostings = await testUser.find().select('jobPostings');
         const allJobPostings = jobPostings.map((user) => user.jobPostings).flat();
         return NextResponse.json({ jobPostings: allJobPostings });
     } catch (err) {
-        return NextResponse.json({ message: "Something went wrong"});
+        return NextResponse.json({ message: 'Something went wrong' }, { status: 400 });
     }
 };
