c06ea68d3d8a9c921e0c5661ab3af8237a1814ba
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-07-25 19:23:26 +0200

    FIX(allPosts):finally fixed error with fetch for all user posts ::rocket::

--- app/api/allPosts/route.ts
+++ app/api/allPosts/route.ts
@@ -1,12 +1,20 @@
-import { NextResponse, NextRequest } from 'next/server';
 import testUser from '@/app/utils/models/userModel';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { NextResponse } from 'next/server';
+import connectionToDB from '@/app/utils/database';
 
-export const GET = async (req: Request, res: NextResponse) => {
+export const POST = async (req: NextApiRequest, res: NextApiResponse) => {
+    connectionToDB();
+
     try {
         const jobPostings = await testUser.find().select('jobPostings');
+        console.log(jobPostings);
         const allJobPostings = jobPostings.map((user) => user.jobPostings).flat();
+        console.log('Job postings:', allJobPostings);
+
         return NextResponse.json({ jobPostings: allJobPostings });
     } catch (err) {
-        return NextResponse.json({ message: 'Something went wrong' }, { status: 400 });
+        console.error('Error in POST route:', err);
+        return NextResponse.json({ message: 'Internal server error' });
     }
 };
