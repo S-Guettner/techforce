a9b635c3c884529a59f1b83589c389dd902342ac
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-07-31 18:53:10 +0200

    changed stxling new post

--- app/bussines/bussines-dashboard/newPost/page.tsx
+++ app/bussines/bussines-dashboard/newPost/page.tsx
@@ -145,7 +145,7 @@ const page: FC<pageProps> = ({ }) => {
                         <div>
                             {tasks && tasks.map((item, index) => {
                                 return (
-                                    <div key={nanoid()}>
+                                    <div className='flex justify-between my-2' key={nanoid()}>
                                         <p className='inline'>&#x2022;{item} </p>
                                         <button className='bg-red-400 p-1 text-white rounded-md' onClick={() => deleteTask(index)}>
                                             <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zm2.46-7.12l1.41-1.41L12 12.59l2.12-2.12 1.41 1.41L13.41 14l2.12 2.12-1.41 1.41L12 15.41l-2.12 2.12-1.41-1.41L10.59 14l-2.13-2.12zM15.5 4l-1-1h-5l-1 1H5v2h14V4z"></path></svg>
@@ -163,7 +163,7 @@ const page: FC<pageProps> = ({ }) => {
                         <div>
                             {offers && offers.map((item, index) => {
                                 return (
-                                    <div key={nanoid()}>
+                                    <div className='flex justify-between my-2' key={nanoid()}>
                                         <p className='inline'>&#x2022;{item} </p>
                                         <button className='bg-red-400 p-1 text-white rounded-md' onClick={() => deleteOffer(index)}>
                                             <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zm2.46-7.12l1.41-1.41L12 12.59l2.12-2.12 1.41 1.41L13.41 14l2.12 2.12-1.41 1.41L12 15.41l-2.12 2.12-1.41-1.41L10.59 14l-2.13-2.12zM15.5 4l-1-1h-5l-1 1H5v2h14V4z"></path></svg>
@@ -181,7 +181,7 @@ const page: FC<pageProps> = ({ }) => {
                         <div>
                             {requirements && requirements.map((item, index) => {
                                 return (
-                                    <div key={nanoid()}>
+                                    <div className='flex justify-between my-2' key={nanoid()}>
                                         <p className='inline'>&#x2022;{item} </p>
                                         <button className='bg-red-400 p-1 text-white rounded-md' onClick={() => deleteRequirement(index)}>
                                             <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zm2.46-7.12l1.41-1.41L12 12.59l2.12-2.12 1.41 1.41L13.41 14l2.12 2.12-1.41 1.41L12 15.41l-2.12 2.12-1.41-1.41L10.59 14l-2.13-2.12zM15.5 4l-1-1h-5l-1 1H5v2h14V4z"></path></svg>
